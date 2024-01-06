1d9a98fdc79ee1ebb9c2d7cafd7e43dc28f9c600
Josh Petrie <josh@joshpetrie.net>
2024-01-06 14:19:36 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,51 @@
+# Flip
+Flip is a Neovim plugin that enables quick navigation between files associated with the current file.
+
+Flip identifies associated file counterparts based on the root name of the file (as with the `:r` filename modifier).
+All files starting with the same root are considered to be counterparts.
+
+## Features
+### Cycle Through Counterparts
+Use `:FlipNext` and `:FlipPrevious` to navigate forward and backward within the counterparts of the current file.
+Flip will try to switch to a window containing the counterpart if one is visible, otherwise it will open the file.
+
+### Define How Counterparts Open
+Both of Flip's commands can take arguments that control how a counterpart will be opened when neccessary. The default
+is to use `:edit`, but you can for example invoke `FlipNext botright vsplit` to open the next counterpart in a vertical
+split to the right.
+
+### Configure Search Paths
+The `paths` option can be set to a list of paths to search when finding counterparts. Non-absolute paths are considered
+relative to the directory of the current file. By default, only the directory of the current file is searched. Flip can
+also be configured to search the directories defined by Neovim's `path` option, and to include listed buffers that
+haven't yet been saved to disk in the search.
+
+## Installation
+Install via your plugin manager of choice. For example, using [`lazy.nvim`](https://github.com/folke/lazy.nvim):
+
+```lua
+{
+  "jpetrie/flip",
+  opts = {
+    -- see below for options
+  }
+}
+```
+
+## Configuration
+Flip comes with the following defaults. See `:help flip.options` for more details.
+
+```lua
+{
+  -- A list of paths to search for counterparts (in addition to the directory of the current file, which is
+  -- always searched).
+  paths = {}, 
+
+  -- If true, also search the directories in Neovim's path (:help path).
+  include_path = false,
+
+  -- If true, also search listed buffers.
+  include_buffers = true,
+}
+```
+
---
+++ lua/flip/init.lua
@@ -0,0 +1,123 @@
+local flip = {
+  options = {
+    paths = {},
+    include_path = false,
+    include_buffers = true,
+  }
+}
+
+local function deduplicate(list)
+  local set = {}
+  for _, item in ipairs(list) do
+    if set[item] == nil then
+      set[item] = {}
+    end
+  end
+
+  local results = {}
+  for item,_ in pairs(set) do
+    table.insert(results, item)
+  end
+
+  table.sort(results)
+  return results
+end
+
+local function get_root(filename)
+  local parts = vim.fn.split(filename, "[.]")
+  local result = parts[1]
+  if filename[1] == "." then
+    -- Restore the leading dot, if it existed.
+    result = "." .. result
+  end
+
+  return result
+end
+
+local function get_counterparts(filename)
+  -- Prepare search paths.
+  local base = vim.fn.fnamemodify(filename, ":p:h")
+  local paths = {base}
+  for _, path in ipairs(flip.options.paths) do
+    if path[1] == "/" then
+      table.insert(paths, path)
+    else
+      table.insert(paths, base .. "/" .. path)
+    end
+  end
+
+  if flip.options.include_path then
+    for _, path in ipairs(vim.opt.path:get()) do
+      if #path > 0 then
+        table.insert(paths, path)
+      end
+    end
+  end
+
+  -- Collapse the search paths into a string, separating each with a comma, for globpath().
+  paths = vim.fn.join(paths, ",")
+
+  -- Locate all potential counterparts on disk.
+  local root = get_root(vim.fn.fnamemodify(filename, ":t"))
+  local files = vim.fn.globpath(paths, root .. "*", 0, 1)
+  local results = {}
+  for _, file in ipairs(files) do
+    table.insert(results, vim.fn.simplify(file))
+  end
+
+  -- Add any potential counterparts from listed buffers that haven't been written to disk yet.
+  if flip.options.include_buffers then
+    for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
+      local is_loaded = vim.api.nvim_buf_is_loaded(buffer)
+      local is_file = vim.bo[buffer].buftype ~= "nofile"
+      if is_loaded and is_file then
+        local buffer_file = vim.api.nvim_buf_get_name(buffer)
+        local buffer_root = get_root(vim.fn.fnamemodify(buffer_file, ":t"))
+        if buffer_root == root then
+          table.insert(results, vim.fn.simplify(buffer_file))
+        end
+      end
+    end
+  end
+
+  results = deduplicate(results)
+  return results
+end
+
+local function cycle(amount, command)
+  local file = vim.api.nvim_buf_get_name(0)
+  local counterparts = get_counterparts(file)
+
+  for index, counterpart in ipairs(counterparts) do
+    if counterpart == file then
+      local which = (index + amount - 1) % #counterparts
+      local target = counterparts[which + 1]
+      if target ~= file then
+        local window = vim.fn.bufwinid(target)
+        if window >= 0 then
+          vim.api.nvim_set_current_win(window)
+          return
+        end
+
+        if #command == 0 then
+          command = "edit"
+        end
+
+        vim.cmd(command .. " " .. vim.fn.fnameescape(target))
+        return
+      end
+    end
+  end
+
+  vim.notify("No counterpart.")
+end
+
+function flip.setup(options)
+  flip.options = vim.tbl_extend("keep", options, flip.options)
+
+  vim.api.nvim_create_user_command("FlipNext", function(command) cycle(1, command.args) end, {nargs = "*"})
+  vim.api.nvim_create_user_command("FlipPrevious", function(command) cycle(-1, command.args) end, {nargs = "*"})
+end
+
+return flip
+
